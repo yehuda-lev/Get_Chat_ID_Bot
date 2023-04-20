27129d8a9e0cb6ddd1f87c7ad756789a3d4af2b7
Yehuda Lev <y124090@gmail.com>
2023-04-20 16:59:16 +0300

    [main] add param admins

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from pyrogram import Client
 import json
 
 from tg.handlers import HANDLERS
+from db import filters as db_filters
+
+configFile = open("config.json")
+config = json.load(configFile)
 
 
 def main():
-    configFile = open("config.json")
-    config = json.load(configFile)
-
     app = Client(config["clientName"], api_id=config["apiId"], api_hash=config["apiHash"],
                  bot_token=config["botToken"],)
 
@@ -22,5 +23,11 @@ def main():
 
 
 if __name__ == '__main__':
+    for admin in config["admins"].split(','):
+        if not db_filters.is_user_exists(tg_id=admin):
+            db_filters.create_user(tg_id=admin, name='admin', admin=True)
+        else:
+            db_filters.change_admin(tg_id=admin, admin=True)
+
     main()
 
