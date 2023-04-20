5fdc9a81c26254ee7d3d4cca71dbcf0dd6542e0d
Yehuda Lev <y124090@gmail.com>
2023-04-20 15:59:36 +0300

    [db.filters] add funcs is_admin and get_users and get_tg_count

--- db/filters.py
+++ db/filters.py
@@ -1,6 +1,6 @@
-from pony.orm import db_session
+from pony.orm import db_session, select
 
-from tables import Users
+from db.tables import Users
 
 
 @db_session
@@ -9,9 +9,23 @@ def is_user_exists(tg_id: int) -> bool:
 
 
 @db_session
-def create_user(tg_id: int, name: str, is_admin=False):
-    role = 'user' if not is_admin else 'admin'
+def create_user(tg_id: int, name: str, admin=False):
+    role = 'user' if not admin else 'admin'
     Users(tg_id=str(tg_id), name=name, role=role)
 
 
+@db_session
+def is_admin(tg_id: int) -> bool:
+    user = Users.get(tg_id=str(tg_id))
+    if user.role == 'admin':
+        return True
+    return False
 
+
+@db_session
+def get_users() -> list[Users]:
+    return select(i.tg_id for i in Users)[:]
+
+
+def get_tg_count() -> int:
+    return len(get_users())
