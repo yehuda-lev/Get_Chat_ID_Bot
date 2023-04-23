ee274ae8fe7976cff2a1bdb118e9acf2ed0b6349
Yehuda Lev <y124090@gmail.com>
2023-04-23 15:34:09 +0300

    [tg.tables] add param in tables 'active' default True

--- db/tables.py
+++ db/tables.py
@@ -1,4 +1,20 @@
-from pony.orm import Database, Required
+# from pony.orm import Database, Required
+#
+# db = Database()
+#
+#
+# class Users(db.Entity):
+#     _table_ = 'users'
+#     tg_id = Required(str, 10)
+#     name = Required(str)
+#     role = Required(str, 5, index=True, default='user')
+#     active = Required(bool, default=True)
+#
+#
+# db.bind(provider='sqlite', filename='get_chat_id.sqlite', create_db=True)
+# db.generate_mapping(create_tables=True)
+
+from pony.orm import Database, Required, db_session
 
 db = Database()
 
@@ -8,7 +24,13 @@ class Users(db.Entity):
     tg_id = Required(str, 10)
     name = Required(str)
     role = Required(str, 5, index=True, default='user')
+    active = Required(bool, default=True)
 
 
 db.bind(provider='sqlite', filename='get_chat_id.sqlite', create_db=True)
-db.generate_mapping(create_tables=True)
+# db.generate_mapping(create_tables=True)
+
+
+with db_session:
+    db.execute('ALTER TABLE users ADD COLUMN active bool DEFAULT True')
+
