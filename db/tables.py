6d4d9c7359ef072803f991880ab2adac5e62c557
Yehuda Lev <y124090@gmail.com>
2023-04-23 17:29:25 +0300

    [db.tables] fix bugs

--- db/tables.py
+++ db/tables.py
@@ -1,20 +1,4 @@
-# from pony.orm import Database, Required
-#
-# db = Database()
-#
-#
-# class Users(db.Entity):
-#     _table_ = 'users'
-#     tg_id = Required(str, 10)
-#     name = Required(str)
-#     role = Required(str, 5, index=True, default='user')
-#     active = Required(bool, default=True)
-#
-#
-# db.bind(provider='sqlite', filename='get_chat_id.sqlite', create_db=True)
-# db.generate_mapping(create_tables=True)
-
-from pony.orm import Database, Required, db_session
+from pony.orm import Database, Required
 
 db = Database()
 
