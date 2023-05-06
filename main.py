4d58c6630e13031c8624ad3329f7fac5e1de590a
Yehuda Lev <y124090@gmail.com>
2023-05-06 23:59:31 +0300

    [main] I changed the login information from 'config.json' to '.env'

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from pyrogram import Client
-import json
 
 from tg.handlers import HANDLERS
 from db import filters as db_filters
 
-configFile = open("config.json")
-config = json.load(configFile)
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def main():
-    app = Client(config["clientName"], api_id=config["apiId"], api_hash=config["apiHash"],
-                 bot_token=config["botToken"],)
-
-    configFile.close()
+    app = Client(name=os.environ['PYROGRAM_NAME_SESSION'], api_id=os.environ['TELEGRAM_API_ID'],
+                 api_hash=os.environ['TELEGRAM_API_HASH'], bot_token=os.environ['TELEGRAM_BOT_TOKEN'])
 
     print(f"Bot {app.name} is up and running!")
 
@@ -23,11 +23,11 @@ def main():
 
 
 if __name__ == '__main__':
-    for admin in config["admins"].split(','):
-        if not db_filters.is_user_exists(tg_id=admin):
-            db_filters.create_user(tg_id=admin, name='admin', admin=True)
+    for admin in os.environ['ADMINS'].split(','):
+        if not db_filters.is_user_exists(tg_id=int(admin)):
+            db_filters.create_user(tg_id=int(admin), name='admin', admin=True)
         else:
-            db_filters.change_admin(tg_id=admin, admin=True)
+            db_filters.change_admin(tg_id=int(admin), admin=True)
 
     main()
 
