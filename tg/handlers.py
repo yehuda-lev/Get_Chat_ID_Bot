8702236752d65c01edbe71d201ec158b04537071
Yehuda Lev <y124090@gmail.com>
2023-05-07 17:00:42 +0300

    [tg.handlers] add strings

--- tg/handlers.py
+++ tg/handlers.py
@@ -7,28 +7,31 @@ from pyrogram.types import User, Chat
 
 from tg import filters as tg_filters
 from tg.admin_command import get_stats, send_message, get_message_for_subscribe
+from tg.strings import get_text
 
 
 async def start(c: Client, msg: types.Message):
-    name = msg.from_user.first_name + \
-           (" " + last if (last := msg.from_user.last_name) else "")
-    text2 = "בבוט זה תוכל לקבל id של קבוצה ערוץ או משתמש"
-    text3 = "בשביל להשתמש בבוט אנא לחצו על הכפתורים למטה ושתפו את הערוץ הקבוצה או המשתמש. - בתגובה הבוט יחזיר לכם את ה ID של הצאט אותו שיתפתם"
-    text4 = "ניתן גם להעביר הודעה לבוט (עם קרדיט) והבוט יחזיר לכם מאיפה ההודעה הועברה."
-    text = f"ברוך הבא {name}\n\n{text2}\n\n{text3}\n\n{text4}"
+    tg_id = msg.from_user.id
+    name = msg.from_user.first_name + (
+        " " + last if (last := msg.from_user.last_name) else "")
+    text2 = get_text('INFO1', tg_id)
+    text3 = get_text('INFO2', tg_id)
+    text4 = get_text('INFO3', tg_id)
+    text = get_text(text='WELCOME', tg_id=tg_id).format(name=name, start2=text2,
+                                                        start3=text3, start4=text4)
     peer = await c.resolve_peer(msg.chat.id)
     await c.invoke(
         SendMessage(peer=peer, message=text, random_id=c.rnd_id(),
                     reply_markup=ReplyKeyboardMarkup(rows=[
                         KeyboardButtonRow(
                             buttons=[
-                                KeyboardButtonRequestPeer(text='משתמש',
+                                KeyboardButtonRequestPeer(text=get_text('USER', tg_id),
                                                           button_id=1,
                                                           peer_type=RequestPeerTypeUser()),
-                                KeyboardButtonRequestPeer(text='קבוצה',
+                                KeyboardButtonRequestPeer(text=get_text('GROUP', tg_id),
                                                           button_id=2,
                                                           peer_type=RequestPeerTypeChat()),
-                                KeyboardButtonRequestPeer(text='ערוץ',
+                                KeyboardButtonRequestPeer(text=get_text('CHANNEL', tg_id),
                                                           button_id=3,
                                                           peer_type=RequestPeerTypeBroadcast())
                             ]
@@ -39,15 +42,16 @@ async def start(c: Client, msg: types.Message):
 
 
 def forward(_, msg: types.Message):
+    tg_id = msg.from_user.id
     if isinstance(msg.forward_from, User):
         # user
-        text = f"ה ID הוא: `{msg.forward_from.id}`"
+        text = get_text('ID_USER', tg_id).format(f'`{msg.forward_from.id}`')
     elif isinstance(msg.forward_from_chat, Chat):
         # channel
-        text = f"ה ID הוא: \u200e`{msg.forward_from_chat.id}`"
+        text = get_text('ID_CHANNEL_OR_GROUP', tg_id).format(f'`{msg.forward_from_chat.id}`')
     elif msg.forward_sender_name:
         # The user hides the forwarding of a message from him or Deleted Account
-        text = f'ה ID מוסתר\n{msg.forward_sender_name}'
+        text = get_text('', tg_id).format(name=msg.forward_sender_name)
     else:
         return
     msg.reply(text=text)
@@ -56,23 +60,28 @@ def forward(_, msg: types.Message):
 async def raw(c: Client, update: UpdateNewMessage, users, chats):
     try:
         if update.message.action.button_id:
+            tg_id = update.message.peer_id.user_id
             button_id = update.message.action.button_id
             chat = update.message.action.peer
             if button_id == 1:
                 # print("user")
+                text = get_text('ID_USER', tg_id).format(f'`{chat.user_id}`')
                 text = f"ה ID הוא: `{chat.user_id}`"
             elif button_id == 2:
                 if isinstance(chat, PeerChat):
                     # print('group')
+                    text = get_text('ID_USER', tg_id).format(f'`{chat.chat_id}`')
                     text = f"ה ID הוא: `{chat.chat_id}`"
                 elif isinstance(chat, PeerChannel):
                     # print('super group')
-                    text = f"ה ID הוא: `\u200e-100{chat.channel_id}`"
+                    text = get_text('ID_CHANNEL_OR_GROUP', tg_id).format(f'`{chat.channel_id}`')
+                    # text = f"ה ID הוא: `\u200e-100{chat.channel_id}`"
                 else:
                     return
             else:
                 # print("channel")
-                text = f"ה ID הוא: `\u200e-100{chat.channel_id}`"
+                text = get_text('ID_CHANNEL_OR_GROUP', tg_id).format(f'`{chat.channel_id}`')
+                # text = f"ה ID הוא: `\u200e-100{chat.channel_id}`"
         else:
             return
         await c.send_message(chat_id=update.message.peer_id.user_id,
