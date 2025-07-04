79f6391c15c6c8201314580c69ead1e4273fe732
yehuda-lev <94082865+yehuda-lev@users.noreply.github.com>
2025-07-04 18:10:54 +0300

    [db] Moved tables to `SQLModel`

--- db/tables.py
+++ db/tables.py
@@ -8,19 +8,14 @@ import logging
 import datetime
 from contextlib import asynccontextmanager
 
-from sqlalchemy import String, ForeignKey
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     create_async_engine,
     async_sessionmaker,
-    AsyncEngine,
 )
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    DeclarativeBase,
-    relationship,
-)
+from sqlalchemy.orm import DeclarativeBase
+
+from sqlmodel import SQLModel, Field, Relationship
 
 
 _logger = logging.getLogger(__name__)
@@ -63,94 +58,96 @@ class BaseTable(DeclarativeBase):
     pass
 
 
-class User(BaseTable):
+class User(SQLModel, table=True):
     """User details"""
 
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    tg_id: Mapped[int] = mapped_column(unique=True)
-    name: Mapped[str] = mapped_column(String(32))
-    username: Mapped[str | None] = mapped_column(String(32))
-    business_id: Mapped[str | None] = mapped_column(String(32))
-    language_code: Mapped[str | None] = mapped_column(String(5))
-    lang: Mapped[str | None] = mapped_column(String(5))  # lang in the bot
+    id: int | None = Field(primary_key=True)
+    tg_id: int = Field(unique=True)
+    name: str = Field(max_length=32)
+    username: str | None = Field(max_length=32)
+    business_id: str | None = Field(max_length=32)
+    language_code: str | None = Field(max_length=5)
+    lang: str | None = Field(max_length=5)  # lang in the bot
 
-    created_at: Mapped[datetime.datetime]
-    created_by: Mapped[str | None] = mapped_column(nullable=True)
-    # updated_at: Mapped[datetime.datetime] = mapped_column(
-    #     default=datetime.datetime.now, onupdate=datetime.datetime.now
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
+    created_by: str | None
+    # updated_at: datetime.datetime = Field(
+    #     default_factory=datetime.datetime.now,
+    #     sa_column_kwargs={"onupdate":datetime.datetime.now}
     # )
 
-    active: Mapped[bool] = mapped_column(default=True)
-    admin: Mapped[bool] = mapped_column(default=False)
-    groups: Mapped[list[Group]] = relationship(back_populates="added_by", lazy="joined")
-    feature: Mapped[Feature] = relationship(back_populates="user", lazy="joined")
+    active: bool = Field(default=True)
+    admin: bool = Field(default=False)
+    groups: list[Group] = Relationship(back_populates="added_by")
+    feature: Feature | None = Relationship(back_populates="user")
 
 
-class Feature(BaseTable):
+class Feature(SQLModel, table=True):
     """Feature details"""
 
     __tablename__ = "feature"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    copy_button: Mapped[bool] = mapped_column(default=True)
-    multiple_chats: Mapped[bool] = mapped_column(default=True)
-    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now)
-    updated_at: Mapped[datetime.datetime] = mapped_column(
-        default=datetime.datetime.now, onupdate=datetime.datetime.now
+    id: int | None = Field(primary_key=True)
+    copy_button: bool = Field(default=True)
+    multiple_chats: bool = Field(default=True)
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
+    updated_at: datetime.datetime = Field(
+        default_factory=datetime.datetime.now,
+        sa_column_kwargs={"onupdate": datetime.datetime.now},
     )
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), unique=True)
-    user: Mapped[User] = relationship("User", back_populates="feature")
+    user_id: int = Field(foreign_key="user.id", unique=True)
+    user: User = Relationship(back_populates="feature")
 
 
-class Group(BaseTable):
+class Group(SQLModel, table=True):
     """Group details"""
 
     __tablename__ = "group"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    group_id: Mapped[int] = mapped_column(unique=True)
-    name: Mapped[str] = mapped_column(String(32))
-    username: Mapped[str | None] = mapped_column(String(32))
-    created_at: Mapped[datetime.datetime]
-    active: Mapped[bool] = mapped_column(default=True)
-    added_by_id: Mapped[int | None] = mapped_column(
-        ForeignKey("user.id"), nullable=True
-    )
-    added_by: Mapped[User | None] = relationship("User", back_populates="groups")
+    id: int | None = Field(primary_key=True)
+    group_id: int = Field(unique=True)
+    name: str = Field(max_length=32)
+    username: str | None = Field(max_length=32)
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
+    active: bool = Field(default=True)
+    added_by_id: int | None = Field(foreign_key="user.id")
+    added_by: User = Relationship(back_populates="groups")
 
 
-class MessageSent(BaseTable):
+class MessageSent(SQLModel, table=True):
     """Sent message details"""
 
     __tablename__ = "message_sent"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    sent_id: Mapped[str] = mapped_column(String(20))
-    message_id: Mapped[int]
-    chat_id: Mapped[int]
-    sent_at: Mapped[datetime.datetime]
+    id: int | None = Field(default=None, primary_key=True)
+    sent_id: str = Field(max_length=20)
+    message_id: int
+    chat_id: int
+    sent_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
 
-class Stats(BaseTable):
+class Stats(SQLModel, table=True):
     """Stats details"""
 
     __tablename__ = "stats"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    type: Mapped[StatsType] = mapped_column(String(32))
-    lang: Mapped[str | None] = mapped_column(String(5))
+    id: int | None = Field(primary_key=True)
+    type: StatsType = Field(
+        sa_column_kwargs={"type": enum.Enum}
+    )  # TODO change to Enum type!
+    lang: str | None = Field(max_length=5)
 
-    created_at: Mapped[datetime.datetime]
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
 
-async def create_tables(engine: AsyncEngine):
+async def create_tables():
     """Create all tables defined in the Base metadata."""
     async with engine.begin() as conn:
-        await conn.run_sync(BaseTable.metadata.create_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(create_tables(engine))
+loop.run_until_complete(create_tables())
